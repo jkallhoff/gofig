b5bb4545183d22f279315282a976d92f5dc43dec
Michael Tighe <striderIIDX@gmail.com>
2013-10-07 11:38:49 -0500

    Adding support for additional datatypes & support for loading a config from a specified path

--- gofig.go
+++ gofig.go
@@ -1,16 +1,25 @@
+// Gofig provides methods to load configuration values from a given JSON file.
 package gofig
 
-//Private vars
-var (
-	loadedConfig *config = new(config)
+import (
+	"encoding/json"
+	"io/ioutil"
 )
 
-//Public functions
-func Str(id string) string {
-	return loadedConfig.Val(id).(string)
+// Config type contains methods to fetch configuration values from a JSON file.
+type Config struct {
+	values map[string]interface{}
 }
 
-//Private functions
-func init() {
-	loadedConfig.Populate()
+// Load creates a new Config type based on the supplied path to the JSON file.
+func Load(path string) (*Config, error) {
+	if b, err := ioutil.ReadFile(path); err != nil {
+		return nil, err
+	} else {
+		c := &Config{}
+		if err = json.Unmarshal(b, &c.values); err != nil {
+			return nil, err
+		}
+		return c, nil
+	}
 }
