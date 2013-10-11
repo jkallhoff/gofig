69f53795c3cf114fc529500b9638891fb9a3c8e7
Michael Tighe <striderIIDX@gmail.com>
2013-10-11 13:02:37 -0500

    Refactor expected test values

--- gofig_test.go
+++ gofig_test.go
@@ -2,6 +2,7 @@ package gofig
 
 import (
 	"encoding/json"
+	"reflect"
 	"regexp"
 	"testing"
 )
@@ -55,73 +56,82 @@ func createTestConfig(t *testing.T) (conf *Config) {
 
 func TestGofigInt(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := 34
 	if i, e := conf.Int("int"); e != nil {
 		t.Errorf("Int() failed to fetch integer value: %v", e)
-	} else if i != 34 {
-		t.Errorf("Unexpected value returned from Int(): expected 34, got %d", i)
+	} else if i != expected {
+		t.Errorf("Unexpected value returned from Int(): expected %d, got %d", expected, i)
 	}
 }
 
 func TestGofigFloat(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := 23.34
 	if f, e := conf.Float("float"); e != nil {
 		t.Errorf("Float() failed to fetch float value: %v", e)
-	} else if f != 23.34 {
-		t.Errorf("Unexpected value returned from Float(): expected 12.34, got %f", f)
+	} else if f != expected {
+		t.Errorf("Unexpected value returned from Float(): expected %f, got %f", expected, f)
 	}
 }
 
 func TestGofigBool(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := true
 	if b, e := conf.Bool("bool"); e != nil {
 		t.Errorf("Bool() failed to fetch boolean value: %v", e)
-	} else if !b {
-		t.Errorf("Unexpected value returned from Bool(): expected true, got %v", b)
+	} else if !expected {
+		t.Errorf("Unexpected value returned from Bool(): expected %v, got %v", expected, b)
 	}
 }
 
 func TestGofigArray(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := []interface{}{1.0, 2.0, 3.0, "Test"}
 	if a, e := conf.Array("array"); e != nil {
 		t.Errorf("Array() failed to fetch array value: %v", e)
-	} else {
-		for i := 0; i < 3; i++ {
-			if int(a[i].(float64)) != (i + 1) {
-				t.Errorf("Unexpected value at element %d, expected %d, got %v.", i, i+1, a[i])
-			}
-		}
-
-		if a[3].(string) != "Test" {
-			t.Errorf("Unexpected value at element 3, expected 'Test', got %v", a[3])
-		}
+	} else if !reflect.DeepEqual(a, expected) {
+		t.Errorf("Unexpected value returned from Array(): expected %v, got %v.", expected, a)
 	}
 }
 
 func TestGofigValidString(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := "value"
 	if s, e := conf.Str("string"); e != nil {
 		t.Errorf("Str() failed to fetch string value: %v", e)
-	} else if s != "value" {
-		t.Errorf("Unexpected value returned from Str(): expected 'value', got %s", s)
+	} else if s != expected {
+		t.Errorf("Unexpected value returned from Str(): expected '%s', got '%s'", expected, s)
 	}
 }
 
 func TestGofigNullString(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := ""
 	if s, e := conf.Str("string2"); e != nil {
 		t.Errorf("Str() failed to fetch string value: %v", e)
-	} else if s != "" {
-		t.Errorf("Unexpected value returned from Str(): expected '', got %s", s)
+	} else if s != expected {
+		t.Errorf("Unexpected value returned from Str(): expected '%s', got '%s'", expected, s)
+	}
+}
+
+func TestGofigNestedStruct(t *testing.T) {
+	conf := createTestConfig(t)
+	obj := &testObj{}
+
+	conf.Struct("obj", obj)
+	if obj.Nested == nil {
+		t.Error("Struct() failed to map nested struct.")
 	}
 }
 
 func TestGofigNullStringInStruct(t *testing.T) {
 	conf := createTestConfig(t)
+	expected := ""
 	obj := &testObj{Nested: &testSubObj{Hah: "should get cleared"}}
 	conf.Struct("obj", obj)
 
-	if obj.Nested.Hah != "" {
-		t.Errorf("Unexpected value when mapping null strings to struct values.")
+	if obj.Nested.Hah != expected {
+		t.Errorf("Unexpected value when mapping null strings to struct values: expected '%s', got '%s'", expected, obj.Nested.Hah)
 	}
 }
 
@@ -129,9 +139,10 @@ func TestGofigStructArray(t *testing.T) {
 	conf := createTestConfig(t)
 	var pairs []testPair
 	conf.StructArray("obj_array", &pairs)
+	expectedLen := 2
 
-	if len(pairs) != 2 {
-		t.Errorf("Failed to map all pairs. expected 2, got %d.", len(pairs))
+	if len(pairs) != expectedLen {
+		t.Errorf("Failed to map all pairs. expected %d, got %d.", expectedLen, len(pairs))
 	}
 
 	for _, p := range pairs {
